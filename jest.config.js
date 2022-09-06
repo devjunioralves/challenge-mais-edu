b23ffbb9f578187a176647d7ddf39a38b0f1ca0f
Junior Alves <jrwanderley17@gmail.com>
2022-09-06 00:51:52 -0300

    chore: configure paths for tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  // roots: ['<rootDir>/src'],
+  rootDir: 'src',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  moduleNameMapper: {
+		'^@application/(.*)$': '<rootDir>/application/$1',
+		'^@config/(.*)$': '<rootDir>/config/$1',
+		'^@di/(.*)$': '<rootDir>/di/$1',
+		'^@domain/(.*)$': '<rootDir>/domain/$1',
+		'^@infrastructure/(.*)$': '<rootDir>/infrastructure/$1',
+		'^@logger/(.*)$': '<rootDir>/logger/$1',
+		'^@presentation/(.*)$': '<rootDir>/presentation/$1',
+		'^@shared/(.*)$': '<rootDir>/shared/$1',
+		'^@src/(.*)$': '<rootDir>/$1',
+	},
 }
